29996edfa0a286e073f0a4ff220b2fcddc17bf3b
Bal Narendra Sapa <bnsapa2000@gmail.com>
2024-01-03 15:49:51 -0500

    add files

---
+++ src/main/java/com/javapp/javapp/JavappApplication.java
@@ -0,0 +1,13 @@
+package com.javapp.javapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javapp/javapp/JavappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapp.javapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
